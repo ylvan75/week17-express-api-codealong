d3a669b35d96100659e15e063b0603023a7755ee
Ylva Nilsson <ylva.z.nilsson@teliacompany.com>
2020-12-06 15:17:48 +0100

    completed the codealong golden globes

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import data from './data.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+console.log(data.length);
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
+
+app.get('/nominations', (req, res) => {
+  res.json(data);
+});
+
+app.get('/years/:year', (req, res) => {
+  const year = req.params.year;
+  console.log({ year });
+  const showWon = req.query.won;
+  console.log(showWon);
+  let nominationsFromYear = data.filter((item) => item.year_award === +year);
+
+  if (showWon) {
+    nominationsFromYear = data.filter((item) => item.win);
+  }
+
+  res.json(nominationsFromYear);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
